1da7fc9b8c80438dd1b08ad4af40dcf700039728
devendraRag <devendra.rag@gmail.com>
2021-04-12 11:44:54 +0530

    remove cart issue

--- app/src/main/java/com/skdirect/activity/MainActivity.java
+++ app/src/main/java/com/skdirect/activity/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    @Overridec
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // check if the request code is same as what is passed  here it is 2
