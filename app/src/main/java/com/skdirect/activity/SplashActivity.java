c9603cf97dbc9d3a7e09da6afba5866423b4ad91
kishortanwar78655 <kishortanwar78655@gmail.com>
2021-04-19 14:09:11 +0530

    splash version code correct

--- app/src/main/java/com/skdirect/activity/SplashActivity.java
+++ app/src/main/java/com/skdirect/activity/SplashActivity.java
@@ -78,22 +78,15 @@ public class SplashActivity extends AppCompatActivity {
             if (appVersionModels != null) {
                 if (appVersionModels.isSuccess()) {
                     if (BuildConfig.VERSION_NAME.equals(appVersionModels.getResultItem().getVersion())) {
-                        if (appVersionModels.isSuccess()) {
-                            if (BuildConfig.VERSION_NAME.equals(appVersionModels.getResultItem().getVersion())) {
-                                SharePrefs.getInstance(activity).putString(SharePrefs.SELLER_URL, appVersionModels.getResultItem().getSellerUrl());
-                                SharePrefs.getInstance(activity).putString(SharePrefs.BUYER_URL, appVersionModels.getResultItem().getBuyerUrl());
-                                SharePrefs.getInstance(activity).putString(SharePrefs.PRIVACY_POLICY, appVersionModels.getResultItem().getPrivacyPolicy());
-                                SharePrefs.getInstance(activity).putString(SharePrefs.TERMS_CONDITION, appVersionModels.getResultItem().getTermsCondition());
-                                SharePrefs.getInstance(activity).putString(SharePrefs.ABOUT_APP, appVersionModels.getResultItem().getAboutApp());
-                                launchHomeScreen();
-                                finish();
-                            } else {
-                                checkVersionData(appVersionModels);
-                            }
-                        }
-                    }else {
+                        SharePrefs.getInstance(activity).putString(SharePrefs.SELLER_URL, appVersionModels.getResultItem().getSellerUrl());
+                        SharePrefs.getInstance(activity).putString(SharePrefs.BUYER_URL, appVersionModels.getResultItem().getBuyerUrl());
+                        SharePrefs.getInstance(activity).putString(SharePrefs.PRIVACY_POLICY, appVersionModels.getResultItem().getPrivacyPolicy());
+                        SharePrefs.getInstance(activity).putString(SharePrefs.TERMS_CONDITION, appVersionModels.getResultItem().getTermsCondition());
+                        SharePrefs.getInstance(activity).putString(SharePrefs.ABOUT_APP, appVersionModels.getResultItem().getAboutApp());
+                        launchHomeScreen();
+                        finish();
+                    } else {
                         checkVersionData(appVersionModels);
-
                     }
                 }
             }
